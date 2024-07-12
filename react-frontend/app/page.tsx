cb42b3694b65bdfb7bfeb9da103627dcf5447e1d
Matthew Gu <jguu0036@student.monash.edu>
2024-07-12 17:17:57 +1000

    Moved about to an individual component

--- react-frontend/app/page.tsx
+++ react-frontend/app/page.tsx
@@ -1,6 +1,7 @@
 import ProjectComponent from './components/Project';
 import Footer from "./components/Footer";
 import TechIcon from './components/TechIcon';
+import About from './components/About';
 
 export default function Home() {
   return (
@@ -34,27 +35,9 @@ export default function Home() {
         </div>
         {/* TODO: A scrolling section indicator on the side of the left sidebar */}
         {/* Main Content */}
-        <div className='flex-1 overflow-auto lg:mt-24 mt-10'>
+        <div className='flex-1 overflow-auto lg:mt-12 mt-10'>
           <div className='flex flex-col justify-center items-centerspace-y-10'>
-            <div id='about' className='page-section mt-4 pt-4'>
-              <h2 className='text-4xl font-bold text-gray-800'>
-                About Me
-              </h2>
-              <div className='mt-4 text-gray-800'>
-                <p className='indent-2 mb-1'>
-                  Hello, I am Jiayi Gu (but I go by 'Matthew'). I'm an undergraduate student at Monash University, Australia, majoring in Electrical Engineering and Computer Science. I'm deeply passionate about the future of technology and the changes it can bring to our world.
-                </p>
-                <p className='indent-2 mb-1'>
-                  With a strong academic record (WAM of 92.72, equivalent to a 4.0 GPA), I am committed to excellence and high performance in everything I do.
-                </p>
-                <p className='indent-2 mb-1'>
-                  My curiosity drives me to explore various technologies. I've worked on a range of projects, from developing full-stack web applications, simulating a CPU using Hardware Description Language (HDL), to designing and building true RMS voltage converters and more.
-                </p>
-                <p className='indent-2 mb-1'>
-                  I'm excited about applying the skills I've learned to make a positive impact and look forward to connecting with like-minded individuals who are also excited about the future of technology. Let's shape the future together!
-                </p>
-              </div>
-            </div>
+            <About />
             <div className='page-section'>
               <h2 className='text-4xl font-bold text-gray-800'>
                 I have worked with...
