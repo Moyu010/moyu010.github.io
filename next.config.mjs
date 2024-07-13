44bdf6f6f67f64a8d20bcddec67679a4de057016
Matthew Gu <jguu0036@student.monash.edu>
2024-07-13 15:18:38 +1000

    Changed action running to manual

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,8 @@ const nextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
     images: {
-        loader: "akamai",
-        path: "",
-    },
-    assetPrefix: "./",
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
